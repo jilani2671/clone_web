e96f125e3414e1d2bf1bb11972876b5c42a3d7f6
jilani2671 <144208021+jilani2671@users.noreply.github.com>
2023-12-26 19:43:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// For Showing hanbb Menu To navbar
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+}
+// close button function/
+
+if(close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+}
